f0cb9555a6b939dd9dfc6077234e1f8b169309e1
LorenaMorenoJimenez <125613923+LorenaMorenoJimenez@users.noreply.github.com>
2023-06-17 21:31:44 +0200

    Delete scripts directory

--- scripts/exito_script.js
+++
@@ -1,9 +0,0 @@
-function mostrarTabla() {
-  var tabla = document.getElementById("tabla");
-  tabla.style.display = "table";
-}
-
-function ocultarTabla() {
-  var tabla = document.getElementById("tabla");
-  tabla.style.display = "none";
-}
